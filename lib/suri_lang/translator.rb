3f41aa79daad3cb4ffdc4662f66b11ec11b98ee6
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-08-29 10:01:41 +0900

    add simple surisuri dictionary

--- lib/suri_lang/translator.rb
+++ lib/suri_lang/translator.rb
@@ -2,9 +2,22 @@ require "natto"
 
 module SuriLang
   class Translator
+    DICTIONARY = {
+      'です': 'デス',
+      'ます': 'マス',
+      'まし': 'マシ',
+      'ませ': 'マセ',
+      'くれ': 'クレ',
+      'ください': 'くだサイ',
+    }
+
     def self.wakati(text)
       nm = Natto::MeCab.new('-Owakati')
       nm.enum_parse(text).select{|n| n.is_nor?}.map(&:surface)
+    end
+
+    def self.katakana(morphemes)
+      morphemes.map {|morpheme| DICTIONARY[morpheme.to_sym] || morpheme}
     end
   end
 end
--- spec/suri_lang/translator_spec.rb
+++ spec/suri_lang/translator_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe SuriLang::Translator do
       expect(SuriLang::Translator.wakati('私の名前は田中です')).to eq(%w(私 の 名前 は 田中 です))
     end
   end
+
+  describe '.katanaka' do
+    it '"です" が "デス" に変換されること' do
+      expect(SuriLang::Translator.katakana(%w(私 の 名前 は 田中 です))).to eq(%w(私 の 名前 は 田中 デス))
+    end
+
+    it '"ました" が "マシた" に変換されること' do
+      expect(SuriLang::Translator.katakana(%w(いよいよ 夏 が やってき まし た))).to eq(%w(いよいよ 夏 が やってき マシ た))
+    end
+
+    it '"してくれ" が "してクレ" に変換されること' do
+      expect(SuriLang::Translator.katakana(%w(ぜひ チェック し て くれ ！))).to eq(%w(ぜひ チェック し て クレ ！))
+    end
+
+    it '"ください" が "くだサイ" に変換されること' do
+      expect(SuriLang::Translator.katakana(%w(そこ に 座っ て ください))).to eq(%w(そこ に 座っ て くだサイ))
+    end
+  end
 end
