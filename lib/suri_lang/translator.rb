871006b30b287f869e158515118446e7518aad9e
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-08-29 09:31:43 +0900

    wakati return Array

--- lib/suri_lang/translator.rb
+++ lib/suri_lang/translator.rb
@@ -4,7 +4,7 @@ module SuriLang
   class Translator
     def self.wakati(text)
       nm = Natto::MeCab.new('-Owakati')
-      nm.parse(text)
+      nm.enum_parse(text).select{|n| n.is_nor?}.map(&:surface)
     end
   end
 end
--- spec/suri_lang/translator_spec.rb
+++ spec/suri_lang/translator_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe SuriLang::Translator do
   describe '.wakati' do
     it '正しく分かち書きされること' do
-      expect(SuriLang::Translator.wakati('私の名前は田中です')).to eq("私 の 名前 は 田中 です \n")
+      expect(SuriLang::Translator.wakati('私の名前は田中です')).to eq(%w(私 の 名前 は 田中 です))
     end
   end
 end
