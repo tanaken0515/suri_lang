ce4bc0b8f2f2ba02be2943607ee26cd79ae5b6c4
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-09-11 10:10:13 +0900

    fix translate method

--- lib/suri_lang/translator.rb
+++ lib/suri_lang/translator.rb
@@ -3,12 +3,12 @@ require "natto"
 module SuriLang
   class Translator
     DICTIONARY = {
-      'です': 'デス',
-      'ます': 'マス',
-      'まし': 'マシ',
-      'ませ': 'マセ',
-      'くれ': 'クレ',
-      'ください': 'くだサイ',
+      'です,助動詞': 'デス',
+      'ます,助動詞': 'マス',
+      'まし,助動詞': 'マシ',
+      'ませ,助動詞': 'マセ',
+      'くれ,動詞': 'クレ',
+      'ください,動詞': 'くだサイ',
     }
 
     def self.wakati(text)
@@ -16,12 +16,15 @@ module SuriLang
       nm.enum_parse(text).select{|n| n.is_nor?}.map(&:surface)
     end
 
-    def self.katakana(morphemes)
-      morphemes.map {|morpheme| DICTIONARY[morpheme.to_sym] || morpheme}
+    def self.katakana(word, word_class)
+      DICTIONARY["#{word},#{word_class}".to_sym] || word
     end
 
     def self.translate(text)
-      return katakana(wakati(text)).join
+      nm = Natto::MeCab.new('-F%m,%f[0]')
+      features = nm.enum_parse(text).select{|n| n.is_nor?}.map{|n| n.feature.split(',')}
+      words = features.map{|feature| katakana(feature[0], feature[1])}
+      words.join
     end
   end
 end
