59558ac66729f4d5ab1f914463c774487aa30240
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-12-12 20:59:04 +0900

    implement random_build

--- lib/suri_lang/greeter.rb
+++ lib/suri_lang/greeter.rb
@@ -2,6 +2,21 @@ require 'yaml'
 
 module SuriLang
   class Greeter
+    attr_reader :message
+    attr_reader :source
+    attr_reader :season
+
+    def initialize(message, source, season)
+      @message = message
+      @source = source
+      @season = season
+    end
+
+    def self.random_build
+      greeting = self.greeting_list.sample
+      self.new(greeting['message'], greeting['source'], greeting['season'])
+    end
+
     def self.greet
       self.greeting_list.sample['message']
     end
--- spec/suri_lang/greeter_spec.rb
+++ spec/suri_lang/greeter_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe SuriLang::Greeter do
       expect(SuriLang::Greeter.greet).to be_a_kind_of(String)
     end
   end
+
+  describe '.random_build' do
+    let(:greeter) { SuriLang::Greeter.random_build }
+
+    it 'Greeterクラスのオブジェクトが作られる' do
+      expect(greeter).to be_a_kind_of(SuriLang::Greeter)
+    end
+
+    it 'attributesが各々正しいこと' do
+      expect(greeter.message).to be_a_kind_of(String)
+      expect(greeter.source).to start_with('https://suzuri.jp/surisurikun/journals')
+      expect(%w[spring summer fall winter none].include?(greeter.season)).to be_truthy
+    end
+  end
 end
